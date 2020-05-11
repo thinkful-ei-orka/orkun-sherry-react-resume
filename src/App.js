78512127c7987c649cedef42d61aff0fc8516178
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-11 11:54:18 -0400

    created app for resume

--- src/App.js
+++ src/App.js
@@ -4,22 +4,57 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <header>
+      <h1>Sally Student</h1>
+      <h2>Computer Programmer</h2>
+    </header>
+    <address>
+      <img className="image" src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="Sally Student, looking mysterious and beautiful" />
+      <p className="name">Sally Suzie Student</p>
+      <br className="email" />Email&colon; <a className="email" href="mailto:sallystudent@example.com" target="_blank">sallystudent@example.com</a>
+      <br className="phone" />Phone&colon; <a className="phone" href="tel:0906451283">(090) 645-1283</a>
+    </address>
+
+    <main className="info">
+
+      <section className="Education InfoSec">
+      <header>
+        <h3>Education&colon;</h3>
       </header>
-    </div>
+        <p>Broad City University
+          <br />BS in Computer Science
+        </p>
+      </section>
+
+      <section className="WorkEXP InfoSec">
+      <header>
+        <h3>Work Experience&colon;</h3>
+      </header>
+          <p>Junior Programmer at Broad City Bank
+            <br />2016 &ndash; 2019
+           <br />Worked alongside senior programmers to develop secure terminals for banking clients
+          </p>
+          <p>Web Developer at Broad City Hall
+            <br />2013 &ndash; 2016
+            <br />Developed funtional and visually appealing websites for the city's projects and interests
+          </p>
+      </section>
+
+      <section className="OtherSkills InfoSec">
+        <header>
+          <h3>Other Skills&colon;</h3>
+        </header>
+       <ul>
+        <li>Microsoft Word</li>
+        <li>Microsoft Excel</li>
+        <li>Beatboxing</li>
+        <li lang="ja">日本語</li>
+      </ul>
+    </section>
+
+    </main>
+    </>
   );
 }
 
